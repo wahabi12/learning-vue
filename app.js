eb5427142309506ddb2d72bc1246e30887a42943
BUSARI LATIFATOU <wahabilatifatou4@gmail.com>
2025-02-13 00:35:42 +0100

    v-bind

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             showBooks: true,
             url: 'https://www.youtube.com/watch?v=CYPZBK8zUik&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=4',
             books: [
-                {title: 'The Final', author: 'latifat', img: 'asset/4.jpg'},
-                {title: 'The Final', author: 'adeola', img: 'asset/5.jpg'},
-                {title: 'The Final', author: 'ajoke', img: 'asset/6 .jpg'}
+                {title: 'The Final', author: 'latifat', img: 'asset/4.jpg', isFav: true},
+                {title: 'The Final', author: 'adeola', img: 'asset/5.jpg', isFav: false},
+                {title: 'The Final', author: 'ajoke', img: 'asset/6.jpg', isFav: true}
             ]  
         }
     },
@@ -14,6 +14,10 @@ const app = Vue.createApp({
         toggleShowBooks() {
             this.showBooks = !this.showBooks
         },
+
+        toggleFav(book){
+            book.isFav = !book.isFav
+        }
         // handleEvent (){
         //     console.log(event);
         // },
