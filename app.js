59d7a39c1723826ff10da13f280abde88c7a478e
BUSARI LATIFATOU <wahabilatifatou4@gmail.com>
2025-02-12 23:14:17 +0100

    @click

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final',
             author: 'Brandon',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent (){
+            console.log(event);
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
-        
+
+
     }
 })
 
